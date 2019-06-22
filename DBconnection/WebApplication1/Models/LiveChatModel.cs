5c6cb7ddb87629ae6aa8a281632393487f91c194
直弘 安孫子 <abi1060special@gmail.com>
2019-06-22 16:28:42 +0900

    model参照をし、viewでチャット内容を繰り返し表示する。

---
+++ DBconnection/WebApplication1/Models/LiveChatModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class LiveChatModel
+    {
+        public string DspMessage { get; set; }
+
+        public string DspName{get;set;}
+
+    }
+}
