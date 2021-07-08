9f556b6a39ac0adb28bd783fbe75024685e0ee72
nao <abi_wase1060@yahoo.co.jp>
2021-07-09 06:23:34 +0900

    [Modify] ボタンの表示やPOSTされた時の挙動を変える

--- GettingYouTubeLiveChat/GettingYouTubeLiveChatWeb/Controllers/HomeController.cs
+++ GettingYouTubeLiveChat/GettingYouTubeLiveChatWeb/Controllers/HomeController.cs
@@ -56,7 +56,43 @@ namespace WebApplication1.Controllers
         /// <returns></returns>
         public ActionResult Search()
         {
+            // 初期表示
+            string display_mode = "INITIAL";
+
+            ViewData["DISPLAY_MODE"] = display_mode;
+
             return View();
+        }
+
+        /// <summary>
+        /// コメント検索結果（POST）
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> Search(string param)
+        {
+            // 再表示
+            string display_mode = "POST";
+
+            if (string.IsNullOrEmpty(param))
+            {
+                display_mode = "INITIAL";
+            }
+
+            ViewData["DISPLAY_MODE"] = display_mode;
+            ViewData["SEARCH_ID"] = param;
+
+            // サービス識別子
+            string service = "Search";
+
+            // YoutubeAPI共通
+            YoutubeAPI youtubeAPI = new YoutubeAPI();
+
+            // APIサービス基本メソッド
+            LiveChatModelList commentModelList = await youtubeAPI.IndexYoutube(param, null, service);
+
+            return View(commentModelList);
         }
 
         /// <summary>
